d452aba2fbdd935b3499b6f0179c86078c0e678c
tanvirch0750 <tanvir0750@yahoo.com>
2022-05-07 15:29:16 +0600

    implemeted jwt functionality

--- index.js
+++ index.js
@@ -12,6 +12,27 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  console.log(authHeader);
+
+  if (!authHeader) {
+    return res.status(401).send({
+      message: "unauthorized access",
+    });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "forbidden access" });
+    } else {
+      console.log("decoded", decoded);
+      req.decoded = decoded;
+      next();
+    }
+  });
+};
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zcq5u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -31,19 +52,36 @@ const run = async () => {
       res.send("welcome to sports inventory server");
     });
 
+    // Auth
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send({ accessToken });
+    });
+
     // GET INVENTORY
     app.get("/inventory", async (req, res) => {
       const email = req.query.email;
-      const query = { email };
+      const decodedEmail = req.decoded.email;
       let cursor;
+
       if (!email) {
         cursor = inventoryCollection.find({});
+        const inventory = await cursor.toArray();
+        res.send(inventory);
       } else {
-        cursor = inventoryCollection.find(query);
+        // cursor = inventoryCollection.find(query);
+        if (email === decodedEmail) {
+          const query = { email };
+          cursor = inventoryCollection.find(query);
+          const inventory = await cursor.toArray();
+          res.send(inventory);
+        } else {
+          res.status(403).send({ message: "forbidden access" });
+        }
       }
-
-      const inventory = await cursor.toArray();
-      res.send(inventory);
     });
 
     // GET SINGLE INVENTORY
@@ -116,12 +154,15 @@ const run = async () => {
       const options = { upsert: true };
       const updatedDoc = {
         $set: {
-          //  name: updatedInventory.name,
-          //  image: updatedInventory.img,
-          //  price: updatedInventory.price,
+          name: updatedInventory.name,
+          image: updatedInventory.img,
+          email: updatedInventory.email,
+          price: updatedInventory.price,
           quantity: updatedInventory.quantity,
-          //  description: updatedInventory.description,
-          //  supplier: updatedInventory.supplier,
+          description: updatedInventory.description,
+          supplier: updatedInventory.supplier,
+          outlet: updatedInventory.outlet,
+          warehouse: updatedInventory.warehouse,
         },
       };
       const result = await inventoryCollection.updateOne(
