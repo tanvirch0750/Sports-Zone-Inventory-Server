10b93972572d555d0ebbfd65cd67d1e383fea1b5
tanvirch0750 <tanvir0750@yahoo.com>
2022-05-07 19:01:08 +0600

    remove cors option

--- index.js
+++ index.js
@@ -8,16 +8,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middlewares
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+app.use(cors());
 app.use(express.json());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
 
 const verifyJWT = (req, res, next) => {
   const authHeader = req.headers.authorization;
